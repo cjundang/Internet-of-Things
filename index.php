ba49beb27dc1ffb8be14df300eecdf0bdbf04e28
Chanankorn Jandaeng <cjundang@gmail.com>
2021-08-13 14:13:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <h1> Chanankorn Jandaeng </h1>
 
-<iframe src="https://thingspeak.com/channels/1458406/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+<iframe width="100px" height="100px" src="https://thingspeak.com/channels/1458406/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
